c2d4bcdc65391e5599189793ff1cb0326f7d1617
Taylor Fouts <fouts.taylor@gmail.com>
2025-05-29 11:32:39 -0400

    Update speed test to include sweep

--- Cluster/suite_squish/tst_speed/test.py
+++ Cluster/suite_squish/tst_speed/test.py
@@ -7,17 +7,29 @@ def main():
     startApplication("ClusterApp")
     
     datainterface = databackend.ws()
-    datainterface.send("speed", 100)
-
-    test.compare(str(waitForObject(names.antares_Cluster_speedLimit_MPH_Rectangle).border.color.name), "#ff0000")
-    test.compare(str(waitForObject(names.antares_Cluster_speedometer_Value_ui).txtMPHvalueFRText), "100")
+    
+    # Test speeds from 50 to 60 mph
+    for speed in range(50, 61):
+        datainterface.send("speed", speed)
+        
+        # Verify the speed value is displayed correctly
+        test.compare(str(waitForObject(names.antares_Cluster_speedometer_Value_ui).txtMPHvalueFRText), str(speed))
+        
+        # Check speed limit border - border color is always red but width changes
+        speed_limit_obj = waitForObject(names.antares_Cluster_speedLimit_MPH_Rectangle)
+        border_width = speed_limit_obj.border.width
+        
+        if speed > 55:
+            # Speed is above limit - should have visible red border (width = 3)
+            test.compare(border_width, 3, f"Speed {speed} mph should show red border")
+        else:
+            # Speed is at or below limit - should have no border (width = 0)
+            test.compare(border_width, 0, f"Speed {speed} mph should not show border")
         
 def waitUntilObjectReady( obj ):
-    highlightObject(obj)
-    snooze(1)
+    highlightObject(obj, 500)
     
 def waitUntilObjectItemReady( obj ):
-    highlightObject(obj)
-    snooze(1)
+    highlightObject(obj, 500)
 
 
